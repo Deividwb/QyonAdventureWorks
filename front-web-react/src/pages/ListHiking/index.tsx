b2ed34d7c0bc87a227f95403cdc5b2eb536796b2
Deividwb <bernardinodeivid@gmail.com>
2022-01-29 21:40:10 -0300

    Creation Component CardHiking

--- front-web-react/src/pages/ListHiking/index.tsx
+++ front-web-react/src/pages/ListHiking/index.tsx
@@ -1,11 +1,44 @@
+import CardHiking from "components/CardHiking";
+import {ReactComponent as RunImg} from 'assets/images/run.svg';
 import "./styles.css";
 
 const ListHiking = () => {
   return (
-      <div>
-          <h2 className="text-center">Lista de Pistas</h2>
-
+    <div>      
+      <h2 className="text-center">Pistas de Corrida</h2>
+      <div className="btn-img">
+        <RunImg/>
       </div>
+      <div className="container my-4">
+        <div className="row">
+
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <CardHiking />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <CardHiking />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <CardHiking />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <CardHiking />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <CardHiking />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            <CardHiking />
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 };
 
